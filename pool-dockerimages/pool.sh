0a9006926371550eb1bcf2ae9455f5cb65a3945b
HenokBerhanu <henokberhanut@gmail.com>
2023-05-25 16:28:13 +0000

    create a shell file to pool images

---
+++ pool-dockerimages/pool.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+docker pull mysql:8.0
+docker pull oaisoftwarealliance/oai-amf:v1.5.0
+docker pull oaisoftwarealliance/oai-nrf:v1.5.0
+docker pull oaisoftwarealliance/oai-smf:v1.5.0
+docker pull oaisoftwarealliance/oai-spgwu-tiny:v1.5.0
+docker pull oaisoftwarealliance/trf-gen-cn5g:focal
+docker pull oaisoftwarealliance/oai-gnb:develop
+docker pull oaisoftwarealliance/oai-nr-ue:develop
