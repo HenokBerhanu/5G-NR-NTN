b88b63f4408b2d88f47ae312c31696daf2bb34d5
HenokBerhanu <henokberhanut@gmail.com>
2023-06-06 08:34:37 +0000

    Updates

--- deploy_RAN/build.gnb.sh
+++ deploy_RAN/build.gnb.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t oai-gnb:develop -f /home/ubuntu/5G-NR-NTN/openairinterface5g/deploy_RAN/docker/Dockerfile.gNB.ubuntu20 .
+docker build -t oai-gnb:develop -f /home/vagrant/5G-NR-NTN/deploy_RAN/docker/Dockerfile.gNB.ubuntu20 .
--- deploy_RAN/build.nrue.sh
+++ deploy_RAN/build.nrue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build -t oai-nr-ue:develop -f /home/ubuntu/5G-NR-NTN/openairinterface5g/deploy_RAN/docker/Dockerfile.nrUE.ubuntu20 .
+docker build -t oai-nr-ue:develop -f /home/vagrant/5G-NR-NTN/deploy_RAN/docker/Dockerfile.nrUE.ubuntu20 .
